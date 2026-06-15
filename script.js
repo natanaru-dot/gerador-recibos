ef02b1aded58ef7533c9b18e29af63a59a6b3014
Natan Oliveira <natan_aru@hotmail.com>
2026-06-15 16:54:38 -0300

    atualização recibos

--- script.js
+++ script.js
@@ -24,99 +24,153 @@ const limparHistorico = document.getElementById("limparHistorico");
 
 const botao = document.getElementById("gerar");
 
-      botao.addEventListener("click", function() {
-        
-const nome = document.getElementById ("nome").value;
+      botao.addEventListener("click", function () {
 
-const valor = document.getElementById ("valor").value;
+                  const nome = document.getElementById("nome").value;
 
-const valorNumero = Number(valor);
+                  const valor = document.getElementById("valor").value;
 
-const servico = document.getElementById ("servico").value;
+                  const valorNumero = Number(valor);
 
-const data = document.getElementById ("data").value;
+                  const servico = document.getElementById("servico").value;
 
-const botao = document.getElementById ("imprimir");
+                  const data = document.getElementById("data").value;
 
-const numeroFormatado = numeroRecibo.toString().padStart(4,"0");
+                  const botao = document.getElementById("imprimir");
 
-
-if (nome == "" || valor == "" || servico == "" || data == "" )
-{
-      alert ("Preencha todos os campos !");
-return;
-}
-
-const nomeFormatado = nome[0].toUpperCase() + nome.slice(1);
-
-const servicoFormatado = servico[0].toUpperCase() + servico.slice(1);
-
-const valorFormatado = valorNumero.toLocaleString("pt-BR", {
-        minimumFractionDigits: 2
-
-});
-
-const resumo = numeroFormatado + " - " + nomeFormatado + " - R$ " + valorFormatado ;
-    
-       historicoRecibos.push(resumo);
-
-      localStorage.setItem(
-            "historicoRecibos",
-
-            JSON.stringify(historicoRecibos)
-  
-      );
-
-document.getElementById("nome").value = "";
-
-document.getElementById("valor").value = "";
-
-document.getElementById("servico").value = "";
-
-document.getElementById("data").value = "";
-
-      botao.addEventListener("click",function() {
-
-        window.print();
-
-});
-
-const partes = data.split("-");
-
-const dataFormatada = 
-partes[2] +"/"+
-partes[1] + "/"+
-partes[0];
+                  const numeroFormatado = numeroRecibo.toString().padStart(4, "0");
 
 
-const recibo = document.getElementById("recibo");
+                  if (nome == "" || valor == "" || servico == "" || data == "") {
+                        alert("Preencha todos os campos !");
+                        return;
+                  }
 
-recibo.innerHTML =
-"<img src='logo.png' id='logo'>" + 
-"<h3>Natan Informatica</h3>" + 
-"<p>Araranguá - SC</P>" + 
-"<p> (48) 99995 - 9046<p>" +
-"<h2>RECIBO Nº " + numeroFormatado + "</h2>" + 
-"Recebi de " + nomeFormatado + 
-" a quantia de R$ " + valorFormatado + 
-" referente ao serviço de " + servicoFormatado + "." +
-"<br><br>Data: " + dataFormatada + 
-"<br><br><br>" +
-"<div id='assinatura'>_______________</div>";
-historico.innerHTML = "<h2>Últimos Recibos</h2>";
-for (let item of historicoRecibos)
-{
-      historico.innerHTML += "<p>" + item + "</p>";
-}
-numeroRecibo++;
-localStorage.setItem("numeroRecebido", numeroRecibo);
-    });
+                  const nomeFormatado = nome[0].toUpperCase() + nome.slice(1);
+
+                  const servicoFormatado = servico[0].toUpperCase() + servico.slice(1);
+
+                  const valorFormatado = valorNumero.toLocaleString("pt-BR", {
+                        minimumFractionDigits: 2
+                  });
+
+                  const resumo = numeroFormatado + 
+                  " - " + 
+                  nomeFormatado + 
+                  " - " +
+                  servicoFormatado +
+                  " - R$ " + 
+                  valorFormatado;
+
+                  const partes = data.split("-");
+
+                  const dataFormatada = partes[2] + "/" +
+                        partes[1] + "/" +
+                        partes[0];
+
+                        const reciboHistorico = {
+
+                        numero: numeroFormatado,
+
+                        nome: nomeFormatado,
+
+                        servico: servicoFormatado,
+
+                        valor: valorFormatado,
+
+                        data: dataFormatada
+
+                         };
+
+
+
+                  historicoRecibos.push(reciboHistorico);
+
+                  localStorage.setItem(
+                        "historicoRecibos",
+
+                        JSON.stringify(historicoRecibos)
+
+                  );
+
+                  document.getElementById("nome").value = "";
+
+                  document.getElementById("valor").value = "";
+
+                  document.getElementById("servico").value = "";
+
+                  document.getElementById("data").value = "";
+
+                  botao.addEventListener("click", function () {
+
+                        window.print();
+
+                  });
+
+                  const recibo = document.getElementById("recibo");
+
+                  recibo.innerHTML =
+                        "<img src='logo.png' id='logo'>" +
+                        "<h3>Natan Informatica</h3>" +
+                        "<p>Araranguá - SC</P>" +
+                        "<p> (48) 99995 - 9046<p>" +
+                        "<h2>RECIBO Nº " + numeroFormatado + "</h2>" +
+                        "Recebi de " + nomeFormatado +
+                        " a quantia de R$ " + valorFormatado +
+                        " referente ao serviço de " + servicoFormatado + "." +
+                        "<br><br>Data: " + dataFormatada +
+                        "<br><br><br>" +
+                        "<div id='assinatura'>_______________</div>";
+                  historico.innerHTML = "<h2>Últimos Recibos</h2>";
+                  for (let i = 0; i < historicoRecibos.length; i++) {
+
+                        let item = historicoRecibos[i];
+
+                        historico.innerHTML +=
+
+                        "<p>" + 
+
+                        "<span onclick='abrirRecibo(" + i + ")'>"+
+                        item.numero + 
+                        " - " + 
+                        item.nome + 
+                        " - " + 
+                        item.servico + 
+                        " - R$ " + 
+                        item.valor + 
+
+                        "</span> " + 
+
+                        "<button class='btnExcluir' onclick='excluirRecibo(" + i + ")'>X</button" + 
+
+                        "</p>";
+                  }
+                  numeroRecibo++;
+                  localStorage.setItem("numeroRecebido", numeroRecibo);
+            });
 
           historico.innerHTML = "<h2>Últimos Recibos</h2>";
 
-          for (let item of historicoRecibos)
+          for (let i = 0; i < historicoRecibos.length; i++)
+
           {
-            historico.innerHTML +="<p>" + item + "</p>";
+
+            let item = historicoRecibos[i];
+
+            historico.innerHTML +=
+                        "<span onclick='abrirRecibo(" + i + ")'>" + 
+                        item.numero + 
+                        " - " + 
+                        item.nome + 
+                        " - " + 
+                        item.servico + 
+                        " - R$ " + 
+                        item.valor + 
+
+                        "</span>" + 
+
+                        "<button class='btnExcluir' onclick='excluirRecibo(" + i + ")'>X</button>" + 
+                        "</p>";
       
           }
 
@@ -175,3 +229,48 @@ exportar.addEventListener("click", function() {
       link.click();
 
 });
+
+function abrirRecibo(indice)
+{
+
+    const item = historicoRecibos[indice];
+
+    recibo.innerHTML =
+
+    "<img src='logo.png' id='logo'>" +
+
+    "<h3>Natan Informatica</h3>" +
+
+    "<p>Araranguá - SC</p>" +
+
+    "<p>(48) 99995 - 9046</p>" +
+
+    "<h2>RECIBO Nº " + item.numero + "</h2>" +
+
+    "Recebi de " + item.nome +
+
+    " a quantia de R$ " + item.valor +
+
+    " referente ao serviço de " + item.servico + "." +
+
+    "<br><br>Data: " + item.data +
+
+    "<br><br><br>" +
+
+    "<div id='assinatura'>_______________</div>";
+
+}
+
+function excluirRecibo(indice) {
+
+      historicoRecibos.splice(indice, 1);
+
+      localStorage.setItem(
+            "historicoRecibos",
+
+            JSON.stringify(historicoRecibos)
+
+      );
+
+      location.reload();
+}
